eadb696f4ab816d0311054141d025ea1831bffcb
Moamen <frontcodeme@gmail.com>
2018-08-21 09:42:13 +0200

    Map has Markers YAAAAY :)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,77 @@
-import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import React, { Component } from 'react'
+import * as dataLocations from './locations.json'
+
 class Map extends Component {
-   render() {
-   const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 35.759465, lng: -5.833954 } }
-        defaultZoom = { 13 }
-      >
-      </GoogleMap>
-   ));
-   return(
-      <div>
-        <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '100%' }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
+  constructor(props) {
+    super(props)
+    this.state = {
+      locations: dataLocations,
+      map: '',
+      markers: [],
+    }
+  }
+
+  componentDidMount() {
+    window.initMap = this.initMap
+    parseHtml('https://maps.googleapis.com/maps/api/js?key=AIzaSyA-cxVbqryrikvhCdgcGhzjEIlC5Pwmz0o&callback=initMap')
+  }
+
+  initMap = () => {
+    const { locations, markers } = this.state
+
+    /* Define the map */
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: { lat: 35.7594651, lng: -5.833954299999999 }
+    })
+
+    /* Keep state in sync */
+    this.setState({
+      map
+  })
+
+    /* Create a marker for each location in the locations.json file */
+    for (let i = 0; i < locations.length; i++) {
+      /* Define the values of the properties */
+      let position = locations[i].position
+      let title = locations[i].title
+      let id = locations[i].key
+
+      /* Create the marker itself */
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: id
+      })
+
+      /* Get those markers into the state */
+      markers.push(marker)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div id="map" role="application"></div>
       </div>
-   );
-   }
-};
-export default Map;
+    )
+  }
+
+}
+
+export default Map
+
+function parseHtml(src) {
+  let ref = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+
+  script.src = src;
+  script.async = true;
+  ref.parentNode.insertBefore(script, ref);
+
+  script.onerror = function () {
+    document.write('Load error: Google Maps couldn\'t load correctly' )
+  };
+}
